c2dd13a326048f890512b3a0083784582252d3c3
DIGIPlv <86823955+DIGIPlv@users.noreply.github.com>
2023-04-04 18:17:10 +0300

    Update for NavBar v2.0 fix

--- 3d_kulturekas.js
+++ 3d_kulturekas.js
@@ -28,7 +28,6 @@
     navBar.append(links);
     // Form mobile menu 
     let mobileMenu = document.getElementsByClassName("nav-bar__container_submenu")[1];
-    console.log(mobileMenu);
     let wrapMobile = document.createElement("div");
     wrapMobile.setAttribute("class", "nav-bar__sublink_narrow");
     let linksMobile = links.cloneNode(true);
